41a8ceea218941e40ea759c9ef674adf1d66978c
azywot <agata.zywot@gmail.com>
2022-12-21 00:13:48 +0100

    combine all detected items on new data

--- src/patterns.py
+++ src/patterns.py
@@ -2,41 +2,42 @@ import numpy as np
 import cv2
 from src.utils import *
 
-def match_patterns(board, templates, color, threshold=0.8):
+def match_patterns(board:np.ndarray, templates:list[str], color:tuple[int], threshold:float=0.8) -> tuple[np.ndarray, list[int]]:
+    coords = list()
     board_matched = board.copy()
+    board_gray = cv2.cvtColor(board_matched, cv2.COLOR_BGR2GRAY)
     for t in templates:
-        template = cv2.imread(f"./data/templates/{t}.jpg", 1)
-        board_gray = cv2.cvtColor(board_matched, cv2.COLOR_BGR2GRAY)
+        template = cv2.imread(t, 1)
         template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
-        w, h = template.shape[0], template.shape[1]
+        h, w = template.shape[0], template.shape[1]
         corr = cv2.matchTemplate(board_gray, template_gray, cv2.TM_CCOEFF_NORMED)
         loc = np.where(corr >= threshold)
 
         for pt in zip(*loc[::-1]):
             cv2.rectangle(board_matched, pt, (pt[0] + w, pt[1] + h), color, 2)
+            coords.append((pt, (pt[0] + w, pt[1] + h)))
 
-    return board_matched
+    return board_matched, coords
 
-def check_match_template(image:np.ndarray,templates:list[np.ndarray], threshold:float=0.8) -> bool:
+def check_match_template(image:np.ndarray, templates:list[np.ndarray], threshold:float=0.8) -> bool:
     for template in templates:
         # image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # template_gray = cv2.cvtColor(t, cv2.COLOR_BGR2GRAY)
+        template_gray = cv2.imread(template, cv2.IMREAD_GRAYSCALE)
         # imshow(image)
-        if image.shape[0] < template.shape[0] or image.shape[1] < template.shape[1]:
-            if image.shape[1] > template.shape[1] or image.shape[0] > template.shape[0]:
+        if image.shape[0] < template_gray.shape[0] or image.shape[1] < template_gray.shape[1]:
+            if image.shape[1] > template_gray.shape[1] or image.shape[0] > template_gray.shape[0]:
                 return False
-            image, template = template, image
-        res = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+            image, template_gray = template_gray, image
+        res = cv2.matchTemplate(image, template_gray, cv2.TM_CCOEFF_NORMED)
         if np.any(np.where(res > threshold)):
             return True
     return False
 
-def get_playing_area(image: np.ndarray, rect_size:tuple[int], pattern_threshold:float=0.6, 
+def get_playing_area(image: np.ndarray, rect_size:tuple[int], templates:list[str], pattern_threshold:float=0.6, 
         color:tuple[int]=(0,0,255), display_steps:bool = False) -> tuple[np.ndarray, list[int]]:
 
     tiles_coords = list()
-    TEMPLATES = [cv2.imread(f"./data/templates/playing_area/{i}.jpg", cv2.IMREAD_GRAYSCALE) for i in range(1,6)]
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # reduce image noise
@@ -56,7 +57,6 @@ def get_playing_area(image: np.ndarray, rect_size:tuple[int], pattern_threshold:
 
     # draw contours on the original image
     image_res = image.copy()
-    pad=2
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         if w < rect_size[0] or w > rect_size[1] or h < rect_size[0] or h > rect_size[1] or w/h > 1.4 or h/w > 1.4:
@@ -67,11 +67,15 @@ def get_playing_area(image: np.ndarray, rect_size:tuple[int], pattern_threshold:
         if is_mostly_white(sub_img_gray):
             cv2.rectangle(image_res, (x, y), (x + w, y + h), color, 2)
             tiles_coords.append(((x, y), (x + w, y + h)))
-        elif check_match_template(sub_img_gray, TEMPLATES, pattern_threshold):
+        elif check_match_template(sub_img_gray, templates, pattern_threshold):
             cv2.rectangle(image_res, (x, y), (x + w, y + h), color, 2)
             tiles_coords.append(((x, y), (x + w, y + h)))
 
+    img, coords = match_patterns(image, templates, color)
+    for c in coords:
+        cv2.rectangle(image_res, c[0], c[1], color, 2)
+        tiles_coords.append((c[0], c[1]))
     if display_steps:
-        imshow(np.concatenate([gray, edges, thresh], 1))
-        imshow(np.concatenate([image, image_res], 1))
+        imshow(np.concatenate([gray, thresh], 1))
+        imshow(cv2.resize(image_res, None, fx=0.8, fy=0.8))
     return image_res, tiles_coords
--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,8 @@ def imshow(a):
       a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
   display(PIL.Image.fromarray(a))
 
-def is_mostly_white(img:np.ndarray, threshold:float=0.7) -> bool:
-    _, img_bin = cv2.threshold(img, 170, 255, cv2.THRESH_BINARY)
+def is_mostly_white(img:np.ndarray, threshold:float=0.6) -> bool:
+    _, img_bin = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
     num_white_pixels = np.sum(img_bin == 255)
     ratio = num_white_pixels / (img_bin.shape[0] * img_bin.shape[1])
     return ratio>=threshold
